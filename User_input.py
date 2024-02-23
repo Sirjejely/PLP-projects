586a2cd71e87e39d5442a1cacfcb28a3aeb938c8
choquann <148155362+choquann@users.noreply.github.com>
2024-02-23 08:04:49 +0100

    My first assignment

---
+++ User_input.py
@@ -0,0 +1,5 @@
+name= input('Please Enter your name: ') #collect user name
+age= input('Please Enter your age: ') #collect user age
+location= input('Please Enter your location: ') #collect user location
+print('Hello {}, you are {} years old and live in {}'.format(name,age,location))
+
